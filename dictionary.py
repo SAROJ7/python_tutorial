aade660464cdae7bc2174175c33d760b16a519d3
saroj7 <sarojsht377@gmail.com>
2021-03-17 15:58:24 +0545

    Dictionary

---
+++ dictionary.py
@@ -0,0 +1,8 @@
+
+student = {'name': 'John', 'age': 25, 'courses': ['Math', 'Science']}
+
+student['phone'] = 9843322391
+print(student['name'])
+print(student['courses'])
+print(student.get('phone', 'Not Found'))
+
