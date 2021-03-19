a2c3765b6d5c48b5c5d0d82759e359f16fb4d143
saroj7 <sarojsht377@gmail.com>
2021-03-19 10:03:29 +0545

    Import Modules

---
+++ import_module_explore_standard_library/intro.py
@@ -0,0 +1,11 @@
+
+from my_module import find_index, test 
+import sys
+
+courses = ['History', 'Math', 'Physics', 'CompSci']
+
+index = find_index(courses, 'CompSci')
+print(index)
+print(test)
+
+print(sys.path)
