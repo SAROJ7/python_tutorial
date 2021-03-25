5208b510381ea13d2d5f753bf777bf959d5010a6
saroj7 <sarojsht377@gmail.com>
2021-03-25 16:28:08 +0545

    Named Tuple

---
+++ named_tuple.py
@@ -0,0 +1,8 @@
+from collections import namedtuple
+
+
+Color = namedtuple('Color', ['red', 'green', 'blue'])
+color = Color(55,155,255)
+dictcolor = Color(255,255,255)
+print(dictcolor.blue)
+print (color.red)
